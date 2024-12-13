62c137e4cf9ef1e4c07045e5786a87ceac67252b
etar125 <inniesharpsoftware@gmail.com>
2024-12-13 22:19:19 +0300

    Исправлен sk.bat

--- sk.bat
+++ sk.bat
@@ -1,16 +1,10 @@
 @echo off
-
 set files=debian.txt arch.txt gentoo.txt slackware.txt other.txt
-
 for %%f in (%files%) do (
-    for /f %%i in ('type "%%f" ^| find /c /v ""') do set /a lines_in_file=%%i
-    echo %%f: %lines_in_file%
+    for /f %%i in ('type "%%f" ^| find /c /v ""') do echo %%f: %%i
 )
-
-echo total:
 for %%f in (%files%) do ( 
     for /f %%i in ('type "%%f" ^| find /c /v ""') do set /a total_lines+=%%i
 )
-echo %total_lines%
-
+echo total: %total_lines%
 pause
