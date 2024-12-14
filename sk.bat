b91dff95ba0fd03c5525d1624bef81e14e31d209
etar125 <inniesharpsoftware@gmail.com>
2024-12-14 15:07:33 +0300

    Объединение списков
    Все списки объединены в один файл
    Обновлены скрипты

--- sk.bat
+++ sk.bat
@@ -1,10 +1,3 @@
 @echo off
-set files=debian.txt arch.txt gentoo.txt slackware.txt fedora.txt other.txt
-for %%f in (%files%) do (
-    for /f %%i in ('type "%%f" ^| find /c /v ""') do echo %%f: %%i
-)
-for %%f in (%files%) do ( 
-    for /f %%i in ('type "%%f" ^| find /c /v ""') do set /a total_lines+=%%i
-)
-echo total: %total_lines%
+for /f %%i in ('type "list.txt" ^| find /c /v ""') do echo total: %%i
 pause
